5171c9a3b49221b2104668c607c93a58cd6f1baf
rcdc1989 <raphaelcapon@gmail.com>
2017-06-19 11:37:38 +0000

    working through intro material

---
+++ snippets.py
@@ -0,0 +1,24 @@
+import logging
+
+# set the log output file and the log level
+
+logging.basicCongic(filename = "snippets.log", level = logging.DEBUG)
+
+def put(name, snippet):
+    """
+    Store a snippet with an associated name.
+
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+    
+def get(name):
+    """Retrieve the snippet with a given name.
+
+    If there is no such snippet, return '404: Snippet Not Found'.
+
+    Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return "404: Snippet Not Found"
